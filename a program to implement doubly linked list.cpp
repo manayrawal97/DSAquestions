b36b71c263ed5ff88d66a660098b4089edd8f86d
manayrawal97 <83336564+manayrawal97@users.noreply.github.com>
2021-10-23 15:51:33 +0530

    Create a program to implement doubly linked list.cpp

---
+++ a program to implement doubly linked list.cpp
@@ -0,0 +1,54 @@
+//Writ a program to implement doubly linked list?
+
+#include<iostream.h>
+#include<stdio.h>
+using namespace std;
+struct Node 
+{
+
+  int data;
+  struct Node *prev;
+  struct Node *next;
+};
+
+struct Node* head = NULL;
+void insert(int newdata) 
+
+{
+  
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+   newnode->data = newdata;
+   newnode->prev = NULL;
+   newnode->next = head;
+   if(head != NULL)
+   head->prev = newnode ;
+   head = newnode;
+}
+
+void display()
+
+{
+   struct Node* ptr;
+   ptr = head;
+   while(ptr != NULL) 
+  
+   {
+      cout<< ptr->data <<" ";
+      ptr = ptr->next;
+   }
+}
+int main() 
+{
+   insert(3);
+   insert(1);
+   insert(7);
+   insert(2);
+   insert(9);
+   cout<<"The doubly linked list is: ";
+
+  display();
+  
+  return 0;
+}
+
+//MANAY RAWAL
